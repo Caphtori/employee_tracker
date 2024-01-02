88dfb2f6e9714482c8034f086621b56444cf6af3
Qohelet <benjy.shuter@gmail.com>
2024-01-01 23:26:39 -0500

    Adds all tables for the database.

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,32 @@
 DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE departmenst (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    dept_name VARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    dept_id INT,
+    FOREIGN KEY (dept_id)
+    REFERENCES departments(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    FOREIGN KEY (manager_id)
+    REFERENCES employees(id)
+    ON DELETE SET NULL
+);
