9cbaa851e33ddd273ef03b70b166f9b5320b538b
Qohelet <benjy.shuter@gmail.com>
2024-01-02 01:54:42 -0500

    Adds seeds for database and fixes errors in mysql code.

--- db/schema.sql
+++ db/schema.sql
@@ -3,15 +3,15 @@ CREATE DATABASE employees_db;
 
 USE employees_db;
 
-CREATE TABLE departmenst (
+CREATE TABLE departments (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    dept_name VARCHAR(30) NOT NULL,
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL,
+    salary DECIMAL(9, 2),
     dept_id INT,
     FOREIGN KEY (dept_id)
     REFERENCES departments(id)
@@ -26,6 +26,7 @@ CREATE TABLE employees (
     manager_id INT,
     FOREIGN KEY (role_id)
     REFERENCES roles(id)
+    ON DELETE SET NULL,
     FOREIGN KEY (manager_id)
     REFERENCES employees(id)
     ON DELETE SET NULL
